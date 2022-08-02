5599950dbc6caf779cbabb4c4879ff9202c85085
Safder12 <109216571+Safder12@users.noreply.github.com>
2022-08-02 21:26:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_name():
- return 'Hello'
+    return 'Hello'
 
 @app.route('/<name>')
 def hello(name):
- return 'Hello ' + name + '!!'
+    return 'Hello ' + name + '!!'
 
-# @app.route('/shah')
-# def hello_shah():
-#  return 'Hello shah'
-
-# @app.route('/')
-# def hello_yawar():
-#  return 'Hello yawar'
 if __name__ == '__main__':
-     app.run()
+    app.run()
